c52ba11d8b3a7f777a421b22573ef591be4052b2
muhammhassan <muhammadhassan7788@gmail.com>
2023-02-13 21:23:03 +0700

    Add Text Style

--- app/src/main/java/com/muhammhassan/reminderobat/ui/theme/Type.kt
+++ app/src/main/java/com/muhammhassan/reminderobat/ui/theme/Type.kt
@@ -2,27 +2,21 @@ package com.muhammhassan.reminderobat.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.muhammhassan.reminderobat.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
+    h1= TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
