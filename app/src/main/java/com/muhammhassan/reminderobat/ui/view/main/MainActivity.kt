dceff7be68265ea92ca3803e0233c809411b6dce
muhammhassan <muhammadhassan7788@gmail.com>
2023-02-13 21:21:24 +0700

    Implement Navigation Component

--- app/src/main/java/com/muhammhassan/reminderobat/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/muhammhassan/reminderobat/ui/view/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.muhammhassan.reminderobat
+package com.muhammhassan.reminderobat.ui.view.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -17,27 +17,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ReminderObatTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainView()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ReminderObatTheme {
-        Greeting("Android")
     }
 }
