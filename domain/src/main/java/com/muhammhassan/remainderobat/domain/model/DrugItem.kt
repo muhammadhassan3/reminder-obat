49f5157e97bc4d50e6a22be230a51b8c64f3e197
muhammhassan <muhammadhassan7788@gmail.com>
2023-02-13 22:16:50 +0700

    Add Domain Model

---
+++ domain/src/main/java/com/muhammhassan/remainderobat/domain/model/DrugItem.kt
@@ -0,0 +1,6 @@
+package com.muhammhassan.remainderobat.domain.model
+
+data class DrugItem(
+    val title: String,
+    val type: String
+)
---
+++ domain/src/main/java/com/muhammhassan/remainderobat/domain/model/GroupedDrugItem.kt
@@ -0,0 +1,6 @@
+package com.muhammhassan.remainderobat.domain.model
+
+data class GroupedDrugItem(
+    val title: String,
+    val item: List<DrugItem>
+)
