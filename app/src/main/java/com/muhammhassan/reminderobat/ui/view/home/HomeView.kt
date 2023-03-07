e5812d93ec0d49d671dea7bf69ad38ebae4dc9ec
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 22:10:40 +0700

    Modify layout

--- app/src/main/java/com/muhammhassan/reminderobat/ui/view/home/HomeView.kt
+++ app/src/main/java/com/muhammhassan/reminderobat/ui/view/home/HomeView.kt
@@ -7,30 +7,37 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
+import androidx.compose.ui.unit.sp
 import com.muhammhassan.reminderobat.ui.component.AlarmGroup
 import com.muhammhassan.reminderobat.ui.component.Articles
 import com.muhammhassan.reminderobat.ui.component.ButtonAddDrug
-import com.muhammhassan.reminderobat.ui.view.Screen
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun HomeView(modifier: Modifier = Modifier, navController: NavHostController) {
+fun HomeView(modifier: Modifier = Modifier, onAddPressed: () -> Unit) {
     val viewModel: HomeViewModel = koinViewModel()
-    val date by viewModel.date
-    val data = viewModel.data
-    val articleData = viewModel.articleData
+    val date by viewModel.date.collectAsState()
+    val data by viewModel.data.collectAsState()
+    val articleData by viewModel.articleData.collectAsState()
 
     val scrollState = rememberLazyListState()
+
+    LaunchedEffect(key1 = true, block = {
+        viewModel.getData()
+    })
+
     Box(modifier = modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxWidth()) {
             Text(
                 modifier = Modifier.align(Alignment.CenterHorizontally),
-                text = "Halo User",
+                text = "Halo, Selamat datang",
                 style = MaterialTheme.typography.h1
             )
             Text(
@@ -40,19 +47,29 @@ fun HomeView(modifier: Modifier = Modifier, navController: NavHostController) {
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = "Alarm akan datang")
-            LazyColumn(
-                modifier = Modifier,
-                state = scrollState,
-                verticalArrangement = Arrangement.spacedBy(4.dp)
-            ) {
-                items(data, key = { it.id }) {
-                    AlarmGroup(item = it)
+            if (data.isNotEmpty()) {
+                LazyColumn(
+                    modifier = Modifier,
+                    state = scrollState,
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    items(data, key = { it.id }) {
+                        AlarmGroup(item = it)
+                    }
                 }
+            } else {
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Kamu belum menambahkan pengingat",
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                )
             }
-            if (articleData.size > 0) Articles(list = articleData)
+            if (articleData.isNotEmpty()) Articles(list = articleData)
         }
         ButtonAddDrug(modifier = Modifier.align(Alignment.BottomEnd)) {
-            navController.navigate(Screen.AddDrugs.route)
+            onAddPressed.invoke()
         }
     }
 }
