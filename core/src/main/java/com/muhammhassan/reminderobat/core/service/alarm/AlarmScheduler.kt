33f62963859dfed6608cbdec92aa640b6abb6246
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 21:23:19 +0700

    Create Alarm Interface

---
+++ core/src/main/java/com/muhammhassan/reminderobat/core/service/alarm/AlarmScheduler.kt
@@ -0,0 +1,12 @@
+package com.muhammhassan.reminderobat.core.service.alarm
+
+import java.util.Date
+
+interface AlarmScheduler {
+    fun scheduleReminder(item: AlarmModel)
+    fun cancelSchedule(scheduleId: Int)
+    fun scheduleStartReminder(drugsId: Int, startDate: Date)
+    fun cancelScheduleStart(drugsId: Int)
+    fun scheduleClearReminder(drugsId: Int, endDate: Date)
+    fun cancelScheduleClear(drugsId: Int)
+}
