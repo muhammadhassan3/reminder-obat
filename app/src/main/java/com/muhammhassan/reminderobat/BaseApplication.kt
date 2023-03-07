3150fce2f0def17c20c42eda493bf104998773cb
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 21:25:12 +0700

    Adding koin module and Timber

--- app/src/main/java/com/muhammhassan/reminderobat/BaseApplication.kt
+++ app/src/main/java/com/muhammhassan/reminderobat/BaseApplication.kt
@@ -1,17 +1,29 @@
 package com.muhammhassan.reminderobat
 
 import android.app.Application
+import com.muhammhassan.reminderobat.core.di.Module.databaseModule
+import com.muhammhassan.reminderobat.core.di.Module.datasourceModule
+import com.muhammhassan.reminderobat.core.di.Module.repositoryModule
 import com.muhammhassan.reminderobat.di.Module.viewModelModule
+import com.muhammhassan.reminderobat.domain.di.Module.useCaseModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class BaseApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
         startKoin {
             androidContext(this@BaseApplication)
             modules(listOf(
-                viewModelModule
+                viewModelModule,
+                useCaseModule,
+                repositoryModule,
+                databaseModule,
+                datasourceModule,
             ))
         }
     }
