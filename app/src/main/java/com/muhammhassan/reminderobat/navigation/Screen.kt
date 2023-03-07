2d146e266112b13ad44763c30e8701defcec5008
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 22:14:39 +0700

    Add ArgsName

--- app/src/main/java/com/muhammhassan/reminderobat/navigation/Screen.kt
+++ app/src/main/java/com/muhammhassan/reminderobat/navigation/Screen.kt
@@ -1,19 +1,18 @@
-package com.muhammhassan.reminderobat.ui.view
-
-import com.muhammhassan.reminderobat.domain.model.DrugsData
+package com.muhammhassan.reminderobat.navigation
 
 sealed class Screen(val route: String){
-    object Splash: Screen("splash")
     object Home: Screen("home")
     object AddDrugs: Screen("add_drugs")
-    object AddReminder: Screen("add_reminder/{${ArgsName.data}}"){
-        fun createRoute(data: DrugsData) = "add_reminder/$data"
-    }
+    object AddReminder: Screen("add_reminder/{${ArgsName.data}}")
     object AddStock: Screen("add_stock")
-    object Detail: Screen("detail")
+    object Detail: Screen("home/{${ArgsName.id}}"){
+        fun createRoute(id: Int) = "home/$id"
+    }
     object Progress: Screen("progress")
 }
 
 object ArgsName{
     const val data = "data"
+    const val id = "id"
 }
+
