15348e359d2deca05c3b3ce5b2f8d68a4d19729f
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 22:15:33 +0700

    Cleaning code

--- app/src/main/java/com/muhammhassan/reminderobat/ui/theme/Type.kt
+++ app/src/main/java/com/muhammhassan/reminderobat/ui/theme/Type.kt
@@ -2,11 +2,9 @@ package com.muhammhassan.reminderobat.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.muhammhassan.reminderobat.R
 
 val Typography = Typography(
     h1= TextStyle(
