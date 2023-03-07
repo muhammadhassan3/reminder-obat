3e5300fedbd326adc6aee76e97fa7648ddca5cd5
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 22:13:16 +0700

    Add Activity

---
+++ app/src/main/java/com/muhammhassan/reminderobat/ui/view/alarm/ReminderDetailActivity.kt
@@ -0,0 +1,34 @@
+package com.muhammhassan.reminderobat.ui.view.alarm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.muhammhassan.reminderobat.ui.theme.ReminderObatTheme
+
+class ReminderDetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val data = intent.data
+        val id = data?.getQueryParameter("id")?.toInt() ?: 0
+        setContent {
+            ReminderObatTheme {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ReminderDetailView(onNavigateUp = {
+                        finish()
+                    }, id = id)
+                }
+            }
+        }
+    }
+}
