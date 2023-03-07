4d6d9f2eba22ac70b105d398b58a3784447da652
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 21:49:45 +0700

    Add property

--- domain/src/main/java/com/muhammhassan/reminderobat/domain/model/DrugItem.kt
+++ domain/src/main/java/com/muhammhassan/reminderobat/domain/model/DrugItem.kt
@@ -1,6 +1,8 @@
 package com.muhammhassan.reminderobat.domain.model
 
 data class DrugItem(
+    val id: Long = 0,
     val title: String,
-    val type: String
+    val type: String,
+    val time: String,
 )
