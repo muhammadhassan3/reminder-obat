4ff79aae800da4fd45be7911afae0cf09fd500df
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 22:14:03 +0700

    Modify dao

--- core/src/main/java/com/muhammhassan/reminderobat/core/database/dao/ScheduleDao.kt
+++ core/src/main/java/com/muhammhassan/reminderobat/core/database/dao/ScheduleDao.kt
@@ -2,19 +2,26 @@ package com.muhammhassan.reminderobat.core.database.dao
 
 import androidx.room.*
 import com.muhammhassan.reminderobat.core.database.entity.ScheduleEntity
+import com.muhammhassan.reminderobat.core.database.relation.ScheduleAndDrug
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ScheduleDao {
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun addDrugs(data: ScheduleEntity)
-
-    @Update
-    fun editDrugs(data: ScheduleEntity)
-
+    fun addAllSchedule(list: List<ScheduleEntity>)
     @Delete
     fun delete(data: ScheduleEntity)
 
-    @Query("select * from schedule")
-    fun getAll(): Flow<List<ScheduleEntity>>
+    @Transaction
+    @Query("select * from schedule where days like :day and time >= :time  order by time asc")
+    fun getNearestSchedule(day: String, time: String): Flow<List<ScheduleAndDrug>>
+
+    @Transaction
+    @Query("select * from schedule where days = :day and time = :time")
+    fun getListSchedule(day: Int, time: String): Flow<List<ScheduleAndDrug>>
+
+    @Transaction
+    @Query("select * from schedule where id = :id")
+    fun getSchedule(id: Long): Flow<ScheduleAndDrug>
 }
