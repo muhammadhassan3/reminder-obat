ebf2cf759add308997810b102def585cf893fd27
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 22:15:52 +0700

    merge with Utils from core module

--- core/src/main/java/com/muhammhassan/reminderobat/core/utils/Utils.kt
+++ core/src/main/java/com/muhammhassan/reminderobat/core/utils/Utils.kt
@@ -4,8 +4,43 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object Utils {
-    fun parseDateToString(date: Date): String{
-        val dateFormater = SimpleDateFormat("yyyyMMdd", Locale("id", "ID"))
+
+    private fun parseDate(pattern: String, date: Date): String {
+        val dateFormater = SimpleDateFormat(pattern, Locale("id", "ID"))
         return dateFormater.format(date)
+    }
+    private fun parseString(pattern: String, date: String): Date {
+        val dateFormater = SimpleDateFormat(pattern, Locale("id", "ID"))
+        return dateFormater.parse(date) ?: throw IllegalArgumentException("Invalid date argument pattern")
+    }
+
+    fun parseDateToString(date: Date): String {
+        return parseDate("yyyyMMdd", date)
+    }
+
+    fun parseStringToDate(date: String): Date{
+        return parseString("yyyyMMdd", date)
+    }
+
+    fun parseTimeToString(hour: Int, minute: Int): String {
+        val hour = if (hour < 10) StringBuilder("0").append(hour) else hour
+        val minute = if (minute < 10) StringBuilder("0").append(minute) else minute
+        return "$hour:$minute"
+    }
+
+    fun parseDateWithDay(date: Date): String{
+        return parseDate("EEEE, dd MMMM yyyy", date)
+    }
+
+    fun parseDateWithoutDayName(date: Date): String{
+        return parseDate("dd MMMM yyyy", date)
+    }
+
+    fun parseStringWithoutDayToDate(date: String): Date{
+        return parseString("dd MMMM yyyy", date)
+    }
+
+    fun parseDateToISO(date: Date): String{
+        return parseDate("yyyy-MM-dd'T'HH:mm:ss.SSSZ", date)
     }
 }
