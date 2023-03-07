613aa876b7d8f71b03f5937911ca5399bfb4b49d
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 22:07:13 +0700

    Modify Table

--- core/src/main/java/com/muhammhassan/reminderobat/core/database/entity/HistoryEntity.kt
+++ core/src/main/java/com/muhammhassan/reminderobat/core/database/entity/HistoryEntity.kt
@@ -6,13 +6,15 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "alarm_history")
 data class HistoryEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val id: Long?,
     val drugName: String,
     val drugsWeight: String,
     val drugsType: String,
-    val time: Long,
+    val time: String,
     val afterEat: Int,
-    val confirmTime: Long,
+    val confirmTime: String,
     val condition: String,
-    val stock: Int
+    val stock: Int,
+    val day: Int,
+    val createdAt: String,
 )
