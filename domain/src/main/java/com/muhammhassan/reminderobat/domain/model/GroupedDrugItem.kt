d381707d4b76aee2598f83791d558bb772ffb932
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 22:06:52 +0700

    Cleaning code

--- domain/src/main/java/com/muhammhassan/reminderobat/domain/model/GroupedDrugItem.kt
+++ domain/src/main/java/com/muhammhassan/reminderobat/domain/model/GroupedDrugItem.kt
@@ -1,7 +1,5 @@
 package com.muhammhassan.reminderobat.domain.model
 
-import com.muhammhassan.reminderobat.domain.model.DrugItem
-
 data class GroupedDrugItem(
     val id: Int,
     val title: String,
