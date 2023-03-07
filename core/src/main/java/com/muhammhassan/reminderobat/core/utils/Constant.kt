b122732e3f2a4bf14d08b6b95aa138d2fce26397
muhammhassan <muhammadhassan7788@gmail.com>
2023-03-07 21:49:09 +0700

    Add constant

--- core/src/main/java/com/muhammhassan/reminderobat/core/utils/Constant.kt
+++ core/src/main/java/com/muhammhassan/reminderobat/core/utils/Constant.kt
@@ -1,7 +1,25 @@
 package com.muhammhassan.reminderobat.core.utils
 
 object Constant {
-    val dbName = "db_drugreminder"
-    val afterEat = 1
-    val beforeEat = 0
+    const val dbName = "db_drugreminder"
+    const val afterEat = 1
+    const val beforeEat = 0
+    const val id = "id"
+    const val title = "title"
+    const val message = "message"
+    const val days = "days"
+    const val channelId = "reminder"
+
+
+}
+
+object Day {
+    const val Sunday = 1
+    const val Monday = 2
+    const val Tuesday = 3
+    const val Wednesday = 4
+    const val Thursday = 5
+    const val Friday = 6
+    const val Saturday = 7
+
 }
