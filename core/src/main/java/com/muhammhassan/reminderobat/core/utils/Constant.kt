0feace9f6998054c94cd5f09f486b3ccb5b5c225
muhammhassan <muhammadhassan7788@gmail.com>
2023-04-16 19:51:39 +0700

    Cleaning file

--- core/src/main/java/com/muhammhassan/reminderobat/core/utils/Constant.kt
+++ core/src/main/java/com/muhammhassan/reminderobat/core/utils/Constant.kt
@@ -1,5 +1,7 @@
 package com.muhammhassan.reminderobat.core.utils
 
+import java.util.*
+
 object Constant {
     const val dbName = "db_drugreminder"
     const val afterEat = 1
@@ -9,17 +11,4 @@ object Constant {
     const val message = "message"
     const val days = "days"
     const val channelId = "reminder"
-
-
-}
-
-object Day {
-    const val Sunday = 1
-    const val Monday = 2
-    const val Tuesday = 3
-    const val Wednesday = 4
-    const val Thursday = 5
-    const val Friday = 6
-    const val Saturday = 7
-
 }
