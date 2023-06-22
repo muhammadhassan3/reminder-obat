c345d7ab6e3084ae3e4f93ad442b24512302a7c8
muhammhassan <muhammadhassan7788@gmail.com>
2023-06-22 09:43:04 +0700

    Add Send Message Function

--- core/src/main/java/com/muhammhassan/reminderobat/core/api/ApiService.kt
+++ core/src/main/java/com/muhammhassan/reminderobat/core/api/ApiService.kt
@@ -2,7 +2,6 @@ package com.muhammhassan.reminderobat.core.api
 
 import com.muhammhassan.reminderobat.core.api.response.Article
 import com.muhammhassan.reminderobat.core.api.response.BaseResponse
-import com.muhammhassan.reminderobat.core.api.response.LoginResponse
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -16,4 +15,7 @@ interface ApiService {
 
     @GET("education")
     suspend fun getEducation(): Response<BaseResponse<List<Article>>>
+    @FormUrlEncoded
+    @POST("/chat")
+    suspend fun sendMessage(@Field("message")message: String): Response<BaseResponse<String>>
 }
