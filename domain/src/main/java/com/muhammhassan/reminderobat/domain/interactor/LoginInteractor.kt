06af61834b66b0e909fa7e290da6516a287cf95d
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-30 08:07:24 +0700

    Add login use case function

--- domain/src/main/java/com/muhammhassan/reminderobat/domain/interactor/LoginInteractor.kt
+++ domain/src/main/java/com/muhammhassan/reminderobat/domain/interactor/LoginInteractor.kt
@@ -1,11 +1,23 @@
 package com.muhammhassan.reminderobat.domain.interactor
 
 import com.muhammhassan.reminderobat.core.repository.UserRepository
+import com.muhammhassan.reminderobat.domain.model.UiState
 import com.muhammhassan.reminderobat.domain.usecase.LoginUseCase
+import com.muhammhassan.reminderobat.domain.utils.Mapper.mapToUi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class LoginInteractor(private val user: UserRepository): LoginUseCase {
 
     override suspend fun saveToken(value: String) {
         user.setToken(value)
+    }
+
+    override suspend fun login(phoneNumber: String): Flow<UiState<String>> {
+        return user.login(phoneNumber).map {
+            it.mapToUi { user ->
+                user.email
+            }
+        }
     }
 }
