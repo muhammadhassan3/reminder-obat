f0077f360e9d22a7a8c31c5c17b1b860edb6b602
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-30 08:05:39 +0700

    Cleaning code

--- domain/src/main/java/com/muhammhassan/reminderobat/domain/interactor/EducationInteractor.kt
+++ domain/src/main/java/com/muhammhassan/reminderobat/domain/interactor/EducationInteractor.kt
@@ -8,16 +8,18 @@ import com.muhammhassan.reminderobat.domain.utils.Mapper.mapToUi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class EducationInteractor(private val education: EducationRepository): EducationUseCase {
+class EducationInteractor(private val education: EducationRepository) : EducationUseCase {
     override fun getData(): Flow<UiState<List<Articles>>> {
         return education.getEducation().map {
-            it.mapToUi{list ->
-                list.map { item -> Articles(
-                    id = item.id,
-                    title = item.title,
-                    content = item.content,
-                    image = item.imageUrl
-                ) }
+            it.mapToUi { list ->
+                list.map { item ->
+                    Articles(
+                        id = item.id,
+                        title = item.title,
+                        content = item.content,
+                        image = item.imageUrl
+                    )
+                }
             }
         }
     }
