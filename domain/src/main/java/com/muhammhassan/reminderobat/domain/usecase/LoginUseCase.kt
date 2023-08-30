5511c28e258a9fe5f799bb15323c5a10cae4d34b
muhammhassan <muhammadhassan7788@gmail.com>
2023-08-30 08:07:46 +0700

    add login function

--- domain/src/main/java/com/muhammhassan/reminderobat/domain/usecase/LoginUseCase.kt
+++ domain/src/main/java/com/muhammhassan/reminderobat/domain/usecase/LoginUseCase.kt
@@ -1,5 +1,10 @@
 package com.muhammhassan.reminderobat.domain.usecase
 
+import com.muhammhassan.reminderobat.domain.model.UiState
+import kotlinx.coroutines.flow.Flow
+
 interface LoginUseCase {
     suspend fun saveToken(value: String)
+
+    suspend fun login(phoneNumber: String): Flow<UiState<String>>
 }
